5cd9189d7eec06aefb0730e4eb5baef8205563eb
BlueBubbleGun <67337238+BlueBubbleGun@users.noreply.github.com>
2020-07-13 15:25:46 +0300

    13.07.2020
    1) Скрипт выбирает из массива слово или словосочетание случайным образом и сохраняет в переменной keyword
    2) Функция writeKeyword заполняет поисковую строку случайным словом/словосочетанием и по завершению кликает на кнопку "найти" 
    3) На странице с результатом поиска ищет необходимую нам ссылку (музыкалка-онлайн.рф) и переходит на нее
    4) Добавлена возможность перемещения по страницам результата поиска
    5) Добавлена возможность перемещения по странице сайта музыкалка-онлайн.рф и выбор рандомной ссылки и перехода по ней.

--- bot_for_yandex.js
+++ bot_for_yandex.js
@@ -10,7 +10,9 @@
 
 let keywords = ["Гобой","Как звучит флейта","Тромбон","Что такое валторна","Фагот","Скрипка","Виолончель"];
 let keyword = keywords[getRandom(0,keywords.length)];
-let button = document.getElementsByClassName("mini-suggest__button")[0];
+let searchBtn = document.getElementsByClassName("mini-suggest__button")[0];
+let nextBtn = document.getElementsByClassName('pager__item')[5];
+let links = document.links;
 
 function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
@@ -19,24 +21,40 @@ function getRandom(min,max){
 function writeKeyword(word){
     let i = 0;
     let timerId = setInterval(()=>{
-        document.getElementsByName("text")[0].value += word[i];
+        text.value += word[i];
         i++;
         if (i == word.length){
             clearInterval(timerId);
-            button.click();
+            searchBtn.click();
             }
     },1000);
 }
 
-if (button.tabIndex != 0)
+if (searchBtn.tabIndex != 0)
     writeKeyword(keyword);
-else{
-let links = document.links;
+else if (location.hostname == "yandex.ru"){
+    let flag = true;
     for (let i = 0; i < links.length; i++){
         if (links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
-            links[i].setAttribute('target', '_self');
+            links[i].removeAttribute("target");
             links[i].click();
             break;
         }
     }
+    if (document.getElementsByClassName('pager__item_kind_page')[0].textContent > 9){
+        flag = false;
+        location.href = "https://yandex.ru/";
+    }
+    if (flag) setTimeout(()=>nextBtn.click(),getRandom(3000,6000));
+}else {
+    setInterval(()=>{
+        if (getRandom(0,101) >= 70) location.href = 'https://yandex.ru/';
+        else{
+            let index = getRandom(0,links.length);
+            if (links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
+                links[index].removeAttribute("target");
+                links[index].click();
+            }
+        }
+    },getRandom(2000,6000));
 }
